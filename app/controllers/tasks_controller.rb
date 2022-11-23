798e046c486a9b466f62f6c0878809cd06add76e
hideki-okawa <h.okawa0303@gmail.com>
2022-11-23 19:01:00 +0900

    タスク新規登録画面と処理の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,8 +6,25 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    # 一覧画面に繊維
+    # flash[:notice]にメッセージを付与
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
 
   def edit
+  end
+
+  private
+
+  def task_params
+    # Strong Parameters
+    # nameとdescrptionのみ抜き取る
+    params.require(:task).permit(:name, :description)
   end
 end
